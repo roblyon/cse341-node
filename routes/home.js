406d45f86ffc008e6fe4f0d175ec9a102e76f370
roblyon <rwlyon@gmail.com>
2022-04-29 12:37:14 -0600

    added contacts route with ugly db code

---
+++ routes/home.js
@@ -0,0 +1,7 @@
+const routes = require('express').Router();
+
+routes.get('/', (req, res) => {
+  res.send('Hello!!! You`re home!!!');
+});
+
+module.exports = routes;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const routes = require('express').Router();
 
-routes.get('/', (req, res) => {
-  res.send('Hello, Sarah!!!');
-});
+routes.use('/', require('./home'));
+routes.use('/contacts', require('./contacts'));
 
 module.exports = routes;
