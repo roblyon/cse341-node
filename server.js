7bbc3a7c18be9dc3abb05a80357c4f669ca78d63
roblyon <rwlyon@gmail.com>
2022-04-29 14:18:32 -0600

    cleaned up our DB code

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const connect = require('./db/connect');
 
-app.use('/', require('./routes'))
+connect.initDatabase();
+
+app.use('/', require('./routes'));
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
-})
+});
